32cb1c2caa702a9a27586d5c68dad3a009b0b271
isangjun <sj27294626@gmail.com>
2016-06-06 03:06:35 +0900

    Refactoring AbstractCollection removeElementAt method

--- src/com/nts/cleancode/collections/AbstractCollection.java
+++ src/com/nts/cleancode/collections/AbstractCollection.java
@@ -2,7 +2,7 @@ package com.nts.cleancode.collections;
 
 public abstract class AbstractCollection {
 	protected static int INITIAL_CAPACITY = 10;
-	protected Object[] elements = new Object[10];
+	protected Object[] elements = new Object[INITIAL_CAPACITY];
 	protected int size = 0;
 	protected boolean readOnly;
 
@@ -66,18 +66,22 @@ public abstract class AbstractCollection {
 			return false;
 		for (int i = 0; i < size; i++)
 			if (elements[i].equals(element)) {
-				elements[i] = null;
-				Object[] newElements = new Object[size - 1];
-				int k = 0;
-				for (int j = 0; j < size; j++) {
-					if (elements[j] != null)
-						newElements[k++] = elements[j];
-				}
-				size--;
-				elements = newElements;
+				removeElementAt(i);
 				return true;
 			}
 		return false;
+	}
+
+	protected void removeElementAt(int i) {
+		elements[i] = null;
+		Object[] newElements = new Object[size - 1];
+		int k = 0;
+		for (int j = 0; j < size; j++) {
+			if (elements[j] != null)
+				newElements[k++] = elements[j];
+		}
+		size--;
+		elements = newElements;
 	}
 	public int capacity() {
 		return elements.length;
